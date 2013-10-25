ce2a50edfd96b6a61ec08df168ea35733d620af2
Laob <tiger@die-elite.de>
2013-10-25 13:18:19 +0200

    Update Welt.java

--- src/Welt.java
+++ src/Welt.java
@@ -1,11 +1,10 @@
-
 public class Welt {
 
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		System.out.println("Hallo schoene Welt");
+		System.out.println("Hallo Du schoene Welt");
 
 	}
 
